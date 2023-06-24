1ec0f602873ccb349d51711c5e5d5ba863f63536
Phoebe Oigara <phoebeoigara@gmail.com>
2023-06-24 23:20:30 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(100))
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.name}, image{self.image}, goes at {self.price}.>'
